ad35a80f0785557552d9214e4687013122e07974
Rohith <rohithnarayan202@gmail.com>
2024-01-13 22:35:51 +0530

    123

--- 123.py
+++ 123.py
@@ -1,10 +1,1 @@
-import csv
-import json
-import requests
-coffeeurl_content = requests.get("https://api.sampleapis.com/coffee/hot")
-coffeeurl_json = coffeeurl_content.json()
-coffee_titles = []
-for coffee_item in coffeeurl_json:
-    coffee_titles.append(coffee_item["title"])
-coffee_titles_sorted = sorted(coffee_titles)
-print(coffee_titles_sorted)
+print("Hi baby")
